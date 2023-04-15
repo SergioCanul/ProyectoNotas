bc79a9c2226721c189070336e3b3b1d8d1921b55
SergioCanul <sergio.18070014@itsmotul.edu.mx>
2023-04-14 18:27:34 -0600

    factories v2

---
+++ database/factories/TopicFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class TopicFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'tema'=>fake()->sentence(),
+            'unidad'=>fake()->randomDigitNot([7,8,9])
+        ];
+    }
+}
