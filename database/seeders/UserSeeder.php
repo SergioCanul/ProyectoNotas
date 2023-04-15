20a59d99e079e92a8c44ca05cc44cfce8c8ee5b1
SergioCanul <sergio.18070014@itsmotul.edu.mx>
2023-04-15 07:58:24 -0600

    seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        /* DB:table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]); */
+        User::factory()
+            ->count(50)
+            //->hasCornellnotes(7)
+            ->create();
+    }
+}
